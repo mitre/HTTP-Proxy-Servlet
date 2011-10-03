f3a4c4c6050be52a416485ad9ce9337833bd6f01
David Smiley <dsmiley@mitre.org>
2011-10-03 18:01:13 -0400

    Basic tests.

--- src/test/java/org/mitre/dsmiley/httpproxy/ProxyServletTest.java
+++ src/test/java/org/mitre/dsmiley/httpproxy/ProxyServletTest.java
@@ -1,32 +1,72 @@
 package org.mitre.dsmiley.httpproxy;
 
+import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.PostMethodWebRequest;
+import com.meterware.httpunit.WebRequest;
+import com.meterware.httpunit.WebResponse;
 import com.meterware.servletunit.ServletRunner;
+import com.meterware.servletunit.ServletUnitClient;
+import org.apache.http.localserver.EchoHandler;
+import org.apache.http.localserver.LocalTestServer;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.xml.sax.SAXException;
 
-import java.util.Hashtable;
+import java.io.IOException;
+import java.util.Properties;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author David Smiley - dsmiley@mitre.org
  */
 public class ProxyServletTest
 {
-//  @Before
-//  public void setUp() throws Exception {
-//
-//  }
-//
-//  @After
-//  public void tearDown() throws Exception {
-//
-//  }
+
+  private LocalTestServer localTestServer;
+  private ServletRunner servletRunner;
+
+  @Before
+  public void setUp() throws Exception {
+    localTestServer = new LocalTestServer(null, null);
+    localTestServer.start();
+
+    servletRunner = new ServletRunner();
+    //servletRunner.
+    Properties params = new Properties();
+    params.setProperty(ProxyServlet.P_PROXY_HOST, "localhost");
+    params.setProperty(ProxyServlet.P_PROXY_PORT, localTestServer.getServiceAddress().getPort()+"");
+    params.setProperty(ProxyServlet.P_PROXY_PATH, "/targetPath");//dummy
+    servletRunner.registerServlet("/proxyMe/*", ProxyServlet.class.getName(), params);//also matches /proxyMe (no pathinfo)
+
+  }
+
+  @After
+  public void tearDown() throws Exception {
+   servletRunner.shutDown();
+   localTestServer.stop();
+  }
 
   @Test
   public void testDoGet() throws Exception {
-    ServletRunner sr = new ServletRunner();
-    Hashtable<String,String> params = new Hashtable<String,String>();
-    params.put(ProxyServlet.P_PROXY_HOST,"localhost");
-    params.put(ProxyServlet.P_PROXY_PORT, "0");//dummy
-    params.put(ProxyServlet.P_PROXY_PATH, "/targetPath");//dummy
-    sr.registerServlet("proxyMe", ProxyServlet.class.getName(), params);
+    localTestServer.register("/targetPath*",new EchoHandler());//matches /targetPath and /targetPath/blahblah
+
+    ServletUnitClient sc = servletRunner.newClient();
+
+    assertEquals("", sc.getResponse( new GetMethodWebRequest( "http://localhost/proxyMe" ) ).getText());
+    assertEquals("", sc.getResponse( new GetMethodWebRequest( "http://localhost/proxyMe/" ) ).getText());
+
+    testEchoRequest(sc, new PostMethodWebRequest( "http://localhost/proxyMe" ));
+    testEchoRequest(sc, new PostMethodWebRequest( "http://localhost/proxyMe/pathInfo" ));
+    testEchoRequest(sc, new PostMethodWebRequest( "http://localhost/proxyMe?def=DEF" ));
+    testEchoRequest(sc, new PostMethodWebRequest( "http://localhost/proxyMe/pathInfo?def=DEF" ));
+  }
+
+  private void testEchoRequest(ServletUnitClient sc, WebRequest request) throws IOException, SAXException {
+    request.setParameter("abc","ABC");
+    WebResponse response = sc.getResponse( request );
+    assertTrue(response.getText().contains("ABC"));
   }
 }
