fb734f6e78f6899a3a449376c49629168b200481
David Smiley <david.w.smiley@gmail.com>
2021-02-23 23:27:19 -0500

    P_TARGET_URI should be public (#192)

--- src/main/java/org/mitre/dsmiley/httpproxy/ProxyServlet.java
+++ src/main/java/org/mitre/dsmiley/httpproxy/ProxyServlet.java
@@ -106,7 +106,8 @@ public class ProxyServlet extends HttpServlet {
   public static final String P_HANDLECOMPRESSION = "handleCompression";
 
   /** The parameter name for the target (destination) URI to proxy to. */
-  protected static final String P_TARGET_URI = "targetUri";
+  public static final String P_TARGET_URI = "targetUri";
+  
   protected static final String ATTR_TARGET_URI =
           ProxyServlet.class.getSimpleName() + ".targetUri";
   protected static final String ATTR_TARGET_HOST =
